840556458d5423b1e070af70b9e726d55c197dae
Dan <daniel.voda@ext.evozon.com>
2025-08-05 14:40:59 +0300

    add Account and Search test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,49 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.microsoft.playwright.*;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Navigation nav = new Navigation(1000);
+
+        // Homepage
+//        nav.navigateHomeQa2();
+//        String title = nav.getPage().title();
+//        String URL = nav.getPage().url();
+//        Locator logoLocator = nav.createLocator("#header > div > a > img.large");
+//        logoLocator.click();
+//        nav.navigateToChelseeTee();
+//        nav.getPage().goBack();
+//        nav.getPage().goForward();
+//        nav.getPage().reload();
+//        nav.navigateClose();
+
+        // Account
+//        nav.navigateHomeQa2();
+//        Locator accountButton = nav.getPage().locator("div[class='account-cart-wrapper']");
+//        accountButton.click();
+//        accountButton = nav.getPage().locator("#header-account > div > ul > li.first > a");
+//        accountButton.click();
+//        nav.navigateClose();
+
+        // Search
+//        nav.navigateHomeQa2();
+//        Locator searchInput = nav.getPage().locator("#search");
+//        searchInput.fill("woman");
+//        Locator searchButton = nav.getPage().locator("#search_mini_form > div.input-box > button");
+//        searchButton.click();
+//        nav.navigateClose();
+
+        // Madison
+        nav.navigateHomeQa2();
+        nav.printProductsInList("body > div > div > div.main-container.col1-layout > div > div > div.std > div.widget.widget-new-products > div.widget-products > ul");
+
+
+
+
+
+
     }
 }
---
+++ src/main/java/org/example/Navigation.java
@@ -0,0 +1,71 @@
+package org.example;
+
+import com.microsoft.playwright.*;
+
+
+public class Navigation {
+
+    private String env="qa2";
+    private Playwright playwright;
+    private Browser browser;
+    private Page page;
+
+    public Navigation(int slowMoValue) {
+        this.playwright = Playwright.create();
+        this.browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(slowMoValue));
+        this.page = browser.newPage();
+    }
+
+    public Page getPage() {
+        return page;
+    }
+
+    public void setPage(Page page) {
+        this.page = page;
+    }
+
+    public Browser getBrowser() {
+        return browser;
+    }
+
+    public void setBrowser(Browser browser) {
+        this.browser = browser;
+    }
+
+    public void navigateHomeQa2(){
+        this.page.navigate("http://qa2magento.dev.evozon.com/");
+        this.env = "qa2";
+    }
+
+    public void navigateHomeQa3(){
+        this.page.navigate("http://qa3magento.dev.evozon.com/");
+        this.env = "qa3";
+    }
+
+    public void navigateToChelseeTee(){
+        this.page.navigate("http://"+this.env+"magento.dev.evozon.com/chelsea-tee-737.html");
+    }
+
+    public void navigateTo(String link) {
+        this.page.navigate(link);
+    }
+
+    public void navigateClose(){
+        this.page.close();
+        this.browser.close();
+        this.playwright.close();
+    }
+
+    public void printProductsInList(String selector){
+        Locator list = this.page.locator("body > div > div > div.main-container.col1-layout > div > div > div.std > div.widget.widget-new-products > div.widget-products > ul");
+        Locator options = list.locator("li");
+        int numberOfProducts = options.count();
+        System.out.println(numberOfProducts + " options in list.");
+        for(int i = 0; i<=numberOfProducts; i++) {
+            String title = list.nth(i).getAttribute("value");
+            System.out.println((i+1)+". "+title);
+        }
+    }
+
+
+}
